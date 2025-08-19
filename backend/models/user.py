f96facd9cf951476e00cb256d3f6ea706d4ac2a5
Nitabushi <biglip.glasses0529@gmail.com>
2025-08-19 12:11:50 +0900

    modelsフォルダ、validatorフォルダと関連ファイルを作成した

---
+++ backend/models/user.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+from . import db
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    # User 1 : N Todo
+    todos = db.relationship("Todo", backref="user", cascade="all, delete-orphan")
